d44311b457d7b990c881e1b78079dce8efabcf96
achahid- <achahid-@c3r2p4.1337.ma>
2023-11-15 14:27:49 +0100

    update

---
+++ ft_calloc.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calloc.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achahid- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/03 15:25:57 by achahid-          #+#    #+#             */
+/*   Updated: 2023/11/03 15:25:59 by achahid-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+/**
+ * ft_calloc - function that allocats enough sapce for count objects that are
+ * size bytes of memory
+ * @count: number of blocks
+ * @size: size of each block
+ * Return: pointer to the allocated memory.
+*/
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	if (size > (SIZE_MAX / count))
+		return (NULL);
+	ptr = (void *)malloc(count * size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, count * size);
+	return (ptr);
+}
+/* #include <stdio.h>
+int main(void)
+{
+ 	int *str;
+ 	int *ptr;
+
+ 	str = ft_calloc(3, sizeof (*str));
+	ptr = calloc(3, sizeof (*ptr));
+ 	printf("%d\n", str[0]);
+ 	printf("%d\n", str[1]);
+ 	printf("%d\n", str[2]);
+ 	printf("%d\n", ptr[0]);
+ 	printf("%d\n", ptr[1]);
+ 	printf("%d\n", ptr[2]);
+ 	free(str);
+ 	return (0);
+} */
