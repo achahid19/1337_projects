870b655ce6ecaeff2b518dd296615c69e2a13c0d
anas chahid ksabi <125438524+anasks19@users.noreply.github.com>
2023-11-15 21:23:21 +0100

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -20,13 +20,13 @@
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	count;
-	char	*ptr;
-	char	*ptr1;
+	unsigned char	*ptr;
+	unsigned char	*ptr1;
 
 	if (!src && !dst)
 		return (NULL);
-	ptr = (char *)src;
-	ptr1 = (char *)dst;
+	ptr = (unsigned char *)src;
+	ptr1 = (unsigned char *)dst;
 	count = 0;
 	while (count < n)
 	{
