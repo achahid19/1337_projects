eba1c9c04ce3f771ddf6f6d8e92f2bd120cfbe9e
achahid19 <anasks1999@gmail.com>
2024-03-06 11:41:22 +0100

    Header file: Edit🛸

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -27,7 +27,7 @@ typedef enum e_bool // define a set of named integer constants.
 typedef struct s_stack_node
 {
 	int					num; // Current number on stack
-	size_t				index; // indexing each element from low to high
+	size_t				index; // indexing each element on stack a
 	int					pos; // position of each element on the stack
 	int					target_pos;
 	int					ca; // cost of a
