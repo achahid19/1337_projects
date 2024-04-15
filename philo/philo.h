413f7298417a20865fa142c561972416bdb82f28
achahid19 <anasks1999@gmail.com>
2024-04-15 10:49:05 +0100

    header file: update

--- philo/philo.h
+++ philo/philo.h
@@ -106,6 +106,6 @@ void	thinking(t_philo *philo);
 
 void	*monitore(void *program);
 
-void	print_msg(char *msg, t_philo *philo, t_bool dead);
+void	print_msg(char *msg, t_philo *philo);
 
 # endif /* PHILO_H */
