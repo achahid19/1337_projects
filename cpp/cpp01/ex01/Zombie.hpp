16168c0eb51198950226aff988b86d58acc9fa33
achahid19 <anasks1999@gmail.com>
2024-09-25 10:52:39 +0100

    Repo restructure: ex00 and ex01

---
+++ cpp/cpp01/ex01/Zombie.hpp
@@ -0,0 +1,17 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie {
+private:
+    std::string name;
+public:
+    ~Zombie( void );
+
+    void		   annouce( void );
+    void           setName( std::string newZombieName );
+    std::string    getName( void );
+};
+
+#endif /* ZOMBIE_HPP */
---
+++ cpp/cpp01/ex01/newZombie.cpp
@@ -0,0 +1,8 @@
+#include "Zombie.h"
+
+Zombie* newZombie( std::string name ) {
+	Zombie* newMob = new Zombie;
+
+	newMob->setName(name);
+	return (newMob);
+};
